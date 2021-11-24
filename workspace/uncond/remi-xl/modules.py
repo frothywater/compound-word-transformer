1415d99758d1ce93abba9036211d846ff99515a6
frothywater <kokoroic@outlook.com>
2021-11-24 08:11:58 +0000

    chores: Clean up code

--- workspace/uncond/remi-xl/modules.py
+++ workspace/uncond/remi-xl/modules.py
@@ -1,8 +1,5 @@
-import sys
 import math
-import functools
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,20 +8,18 @@ import torch.nn.functional as F
 class PositionalEmbedding(nn.Module):
     def __init__(self, demb):
         super(PositionalEmbedding, self).__init__()
-
         self.demb = demb
-
         inv_freq = 1 / (10000 ** (torch.arange(0.0, demb, 2.0) / demb))
-        self.register_buffer('inv_freq', inv_freq)
+        self.register_buffer("inv_freq", inv_freq)
 
     def forward(self, pos_seq, bsz=None):
         sinusoid_inp = torch.ger(pos_seq, self.inv_freq)
         pos_emb = torch.cat([sinusoid_inp.sin(), sinusoid_inp.cos()], dim=-1)
 
         if bsz is not None:
-            return pos_emb[:,None,:].expand(-1, bsz, -1)
+            return pos_emb[:, None, :].expand(-1, bsz, -1)
         else:
-            return pos_emb[:,None,:]
+            return pos_emb[:, None, :]
 
 
 class PositionwiseFF(nn.Module):
@@ -36,36 +31,34 @@ class PositionwiseFF(nn.Module):
         self.dropout = dropout
 
         self.CoreNet = nn.Sequential(
-            nn.Linear(d_model, d_inner), nn.ReLU(inplace=True),
+            nn.Linear(d_model, d_inner),
+            nn.ReLU(inplace=True),
             nn.Dropout(dropout),
             nn.Linear(d_inner, d_model),
             nn.Dropout(dropout),
         )
 
         self.layer_norm = nn.LayerNorm(d_model)
-
         self.pre_lnorm = pre_lnorm
 
     def forward(self, inp):
         if self.pre_lnorm:
             ##### layer normalization + positionwise feed-forward
             core_out = self.CoreNet(self.layer_norm(inp))
-
             ##### residual connection
             output = core_out + inp
         else:
             ##### positionwise feed-forward
             core_out = self.CoreNet(inp)
-
             ##### residual connection + layer normalization
             output = self.layer_norm(inp + core_out)
-
         return output
 
 
 class RelMultiHeadAttn(nn.Module):
-    def __init__(self, n_head, d_model, d_head, dropout, dropatt=0,
-                 tgt_len=None, ext_len=None, mem_len=None, pre_lnorm=False):
+    def __init__(
+        self, n_head, d_model, d_head, dropout, dropatt=0, tgt_len=None, ext_len=None, mem_len=None, pre_lnorm=False
+    ):
         super(RelMultiHeadAttn, self).__init__()
 
         self.n_head = n_head
@@ -88,8 +81,8 @@ class RelMultiHeadAttn(nn.Module):
     def _parallelogram_mask(self, h, w, left=False):
         mask = torch.ones((h, w)).byte()
         m = min(h, w)
-        mask[:m,:m] = torch.triu(mask[:m,:m])
-        mask[-m:,-m:] = torch.tril(mask[-m:,-m:])
+        mask[:m, :m] = torch.triu(mask[:m, :m])
+        mask[-m:, -m:] = torch.tril(mask[-m:, -m:])
 
         if left:
             return mask
@@ -98,8 +91,7 @@ class RelMultiHeadAttn(nn.Module):
 
     def _shift(self, x, qlen, klen, mask, left=False):
         if qlen > 1:
-            zero_pad = torch.zeros((x.size(0), qlen-1, x.size(2), x.size(3)),
-                                    device=x.device, dtype=x.dtype)
+            zero_pad = torch.zeros((x.size(0), qlen - 1, x.size(2), x.size(3)), device=x.device, dtype=x.dtype)
         else:
             zero_pad = torch.zeros(0, device=x.device, dtype=x.dtype)
 
@@ -109,14 +101,12 @@ class RelMultiHeadAttn(nn.Module):
         else:
             x_padded = torch.cat([x, zero_pad], dim=1).expand(qlen, -1, -1, -1)
 
-        x = x_padded.masked_select(mask[:,:,None,None]) \
-                    .view(qlen, klen, x.size(2), x.size(3))
+        x = x_padded.masked_select(mask[:, :, None, None]).view(qlen, klen, x.size(2), x.size(3))
 
         return x
 
     def _rel_shift(self, x, zero_triu=False):
-        zero_pad = torch.zeros((x.size(0), 1, *x.size()[2:]),
-                               device=x.device, dtype=x.dtype)
+        zero_pad = torch.zeros((x.size(0), 1, *x.size()[2:]), device=x.device, dtype=x.dtype)
         x_padded = torch.cat([zero_pad, x], dim=1)
 
         x_padded = x_padded.view(x.size(1) + 1, x.size(0), *x.size()[2:])
@@ -125,12 +115,13 @@ class RelMultiHeadAttn(nn.Module):
 
         if zero_triu:
             ones = torch.ones((x.size(0), x.size(1)))
-            x = x * torch.tril(ones, x.size(1) - x.size(0))[:,:,None,None]
+            x = x * torch.tril(ones, x.size(1) - x.size(0))[:, :, None, None]
 
         return x
 
     def forward(self, w, r, attn_mask=None, mems=None):
         raise NotImplementedError
+
 
 class RelPartialLearnableMultiHeadAttn(RelMultiHeadAttn):
     def __init__(self, *args, **kwargs):
@@ -164,18 +155,18 @@ class RelPartialLearnableMultiHeadAttn(RelMultiHeadAttn):
 
         klen = w_head_k.size(0)
 
-        w_head_q = w_head_q.view(qlen, bsz, self.n_head, self.d_head)           # qlen x bsz x n_head x d_head
-        w_head_k = w_head_k.view(klen, bsz, self.n_head, self.d_head)           # qlen x bsz x n_head x d_head
-        w_head_v = w_head_v.view(klen, bsz, self.n_head, self.d_head)           # qlen x bsz x n_head x d_head
+        w_head_q = w_head_q.view(qlen, bsz, self.n_head, self.d_head)  # qlen x bsz x n_head x d_head
+        w_head_k = w_head_k.view(klen, bsz, self.n_head, self.d_head)  # qlen x bsz x n_head x d_head
+        w_head_v = w_head_v.view(klen, bsz, self.n_head, self.d_head)  # qlen x bsz x n_head x d_head
 
-        r_head_k = r_head_k.view(rlen, self.n_head, self.d_head)                # qlen x n_head x d_head
+        r_head_k = r_head_k.view(rlen, self.n_head, self.d_head)  # qlen x n_head x d_head
 
         #### compute attention score
-        rw_head_q = w_head_q + r_w_bias                                         # qlen x bsz x n_head x d_head
-        AC = torch.einsum('ibnd,jbnd->ijbn', (rw_head_q, w_head_k))             # qlen x klen x bsz x n_head
+        rw_head_q = w_head_q + r_w_bias  # qlen x bsz x n_head x d_head
+        AC = torch.einsum("ibnd,jbnd->ijbn", (rw_head_q, w_head_k))  # qlen x klen x bsz x n_head
 
         rr_head_q = w_head_q + r_r_bias
-        BD = torch.einsum('ibnd,jnd->ijbn', (rr_head_q, r_head_k))              # qlen x klen x bsz x n_head
+        BD = torch.einsum("ibnd,jnd->ijbn", (rr_head_q, r_head_k))  # qlen x klen x bsz x n_head
         BD = self._rel_shift(BD)
 
         # [qlen x klen x bsz x n_head]
@@ -183,25 +174,22 @@ class RelPartialLearnableMultiHeadAttn(RelMultiHeadAttn):
         attn_score.mul_(self.scale)
 
         #### compute attention probability
-        
+
         if attn_mask is not None and attn_mask.any().item():
             if attn_mask.dim() == 2:
-                attn_score = attn_score.float().masked_fill(
-                    attn_mask[None,:,:,None], -float('inf')).type_as(attn_score)
+                attn_score = attn_score.float().masked_fill(attn_mask[None, :, :, None], -float("inf")).type_as(attn_score)
             elif attn_mask.dim() == 3:
-                attn_score = attn_score.float().masked_fill(
-                    attn_mask[:,:,:,None], -float('inf')).type_as(attn_score)
+                attn_score = attn_score.float().masked_fill(attn_mask[:, :, :, None], -float("inf")).type_as(attn_score)
 
         # [qlen x klen x bsz x n_head]
         attn_prob = F.softmax(attn_score, dim=1)
         attn_prob = self.dropatt(attn_prob)
 
         #### compute attention vector
-        attn_vec = torch.einsum('ijbn,jbnd->ibnd', (attn_prob, w_head_v))
+        attn_vec = torch.einsum("ijbn,jbnd->ibnd", (attn_prob, w_head_v))
 
         # [qlen x bsz x n_head x d_head]
-        attn_vec = attn_vec.contiguous().view(
-            attn_vec.size(0), attn_vec.size(1), self.n_head * self.d_head)
+        attn_vec = attn_vec.contiguous().view(attn_vec.size(0), attn_vec.size(1), self.n_head * self.d_head)
 
         ##### linear projection
         attn_out = self.o_net(attn_vec)
@@ -218,20 +206,15 @@ class RelPartialLearnableMultiHeadAttn(RelMultiHeadAttn):
 
 
 class RelPartialLearnableDecoderLayer(nn.Module):
-    def __init__(self, n_head, d_model, d_head, d_inner, dropout,
-                 **kwargs):
+    def __init__(self, n_head, d_model, d_head, d_inner, dropout, **kwargs):
         super(RelPartialLearnableDecoderLayer, self).__init__()
 
-        self.dec_attn = RelPartialLearnableMultiHeadAttn(n_head, d_model,
-                            d_head, dropout, **kwargs)
-        self.pos_ff = PositionwiseFF(d_model, d_inner, dropout, 
-                                     pre_lnorm=kwargs.get('pre_lnorm'))
+        self.dec_attn = RelPartialLearnableMultiHeadAttn(n_head, d_model, d_head, dropout, **kwargs)
+        self.pos_ff = PositionwiseFF(d_model, d_inner, dropout, pre_lnorm=kwargs.get("pre_lnorm"))
 
     def forward(self, dec_inp, r, r_w_bias, r_r_bias, dec_attn_mask=None, mems=None):
 
-        output = self.dec_attn(dec_inp, r, r_w_bias, r_r_bias,
-                               attn_mask=dec_attn_mask,
-                               mems=mems)
+        output = self.dec_attn(dec_inp, r, r_w_bias, r_r_bias, attn_mask=dec_attn_mask, mems=mems)
         output = self.pos_ff(output)
 
         return output
@@ -247,38 +230,35 @@ class Embeddings(nn.Module):
         return self.lut(x) * math.sqrt(self.d_model)
 
 
-
 class MemTransformerLM(nn.Module):
-    def __init__(self, modelConfig, n_token,
-                tie_projs=[False], cutoffs=[], 
-                 is_training=True):
+    def __init__(self, modelConfig, n_token, tie_projs=[False], cutoffs=[], is_training=True):
         super(MemTransformerLM, self).__init__()
 
         self.n_token = n_token
-        self.n_layer= modelConfig['n_layer']
-        self.n_head= modelConfig['n_head']
-        self.d_model = modelConfig['d_model']
-        self.d_embed = d_model if modelConfig['d_embed'] is None else modelConfig['d_embed']
+        self.n_layer = modelConfig["n_layer"]
+        self.n_head = modelConfig["n_head"]
+        self.d_model = modelConfig["d_model"]
+        self.d_embed = d_model if modelConfig["d_embed"] is None else modelConfig["d_embed"]
         self.d_head = self.d_model // self.n_head
-        self.d_inner= modelConfig['d_inner']
+        self.d_inner = modelConfig["d_inner"]
 
-        self.mem_len = modelConfig['mem_len']
-        self.tgt_len = modelConfig['tgt_len']
-        self.ext_len = modelConfig['ext_len']
-        self.max_klen = self.tgt_len + self.ext_len + self.mem_len  #70+0+512
+        self.mem_len = modelConfig["mem_len"]
+        self.tgt_len = modelConfig["tgt_len"]
+        self.ext_len = modelConfig["ext_len"]
+        self.max_klen = self.tgt_len + self.ext_len + self.mem_len  # 70+0+512
 
-        self.dropout= modelConfig['dropout']
-        self.dropatt = modelConfig['dropatt']
+        self.dropout = modelConfig["dropout"]
+        self.dropatt = modelConfig["dropatt"]
 
-        self.clamp_len = modelConfig['clamp_len']
-        self.div_val = modelConfig['div_val']
+        self.clamp_len = modelConfig["clamp_len"]
+        self.div_val = modelConfig["div_val"]
 
-        #choice
-        self.pre_lnorm = modelConfig['pre_lnorm']
-        self.same_length = modelConfig['same_length']
+        # choice
+        self.pre_lnorm = modelConfig["pre_lnorm"]
+        self.same_length = modelConfig["same_length"]
         self.is_training = is_training
 
-        #building layers
+        # building layers
         self.drop = nn.Dropout(self.dropout)
         self.word_emb = Embeddings(self.n_token, self.d_model)
 
@@ -286,24 +266,32 @@ class MemTransformerLM(nn.Module):
         for i in range(self.n_layer):
             self.layers.append(
                 RelPartialLearnableDecoderLayer(
-                    self.n_head, self.d_model, self.d_head, self.d_inner, self.dropout,
-                    tgt_len=self.tgt_len, ext_len=self.ext_len, mem_len=self.mem_len,
-                    dropatt=self.dropatt, pre_lnorm=self.pre_lnorm)
+                    self.n_head,
+                    self.d_model,
+                    self.d_head,
+                    self.d_inner,
+                    self.dropout,
+                    tgt_len=self.tgt_len,
+                    ext_len=self.ext_len,
+                    mem_len=self.mem_len,
+                    dropatt=self.dropatt,
+                    pre_lnorm=self.pre_lnorm,
+                )
             )
 
         # output layer
         self.linear_proj = nn.Linear(self.d_model, self.n_token)
 
         # loss
-        self.loss_func = nn.CrossEntropyLoss(reduction='none')
+        self.loss_func = nn.CrossEntropyLoss(reduction="none")
         self._create_params()
 
     def compute_loss(self, predict, target, loss_mask=None):
-        '''
+        """
         predict, target,
         input:  (N, C, ...)
         target: (N, ...)
-        '''
+        """
         loss = self.loss_func(predict, target)
         loss = loss * loss_mask
         loss = torch.sum(loss) / torch.sum(loss_mask)
@@ -323,7 +311,7 @@ class MemTransformerLM(nn.Module):
         if self.mem_len > 0:
             mems = []
             param = next(self.parameters())
-            for i in range(self.n_layer+1):
+            for i in range(self.n_layer + 1):
                 empty = torch.empty(0, dtype=param.dtype, device=param.device)
                 mems.append(empty)
             return mems
@@ -331,8 +319,9 @@ class MemTransformerLM(nn.Module):
             return None
 
     def _update_mems(self, hids, mems, mlen, qlen):
-        
-        if mems is None: return None
+
+        if mems is None:
+            return None
         # mems is not None
         # assert len(hids) == len(mems), 'len(hids) != len(mems)'
 
@@ -352,13 +341,11 @@ class MemTransformerLM(nn.Module):
 
         return new_mems
 
-
-
     def _forward(self, dec_inp, mems=None):
-        '''
+        """
         output of _forward: step x batch x n_feat
         predict = self.linear_proj(hidden)
-        '''
+        """
 
         qlen, bsz = dec_inp.size()
         mlen = mems[0].size(0) if mems is not None else 0
@@ -374,16 +361,12 @@ class MemTransformerLM(nn.Module):
                 mask_shift_len = qlen - mask_len
             else:
                 mask_shift_len = qlen
-            dec_attn_mask = (torch.triu(all_ones, 1+mlen)
-                    + torch.tril(all_ones, -mask_shift_len)).bool()[:, :, None] # -1
+            dec_attn_mask = (torch.triu(all_ones, 1 + mlen) + torch.tril(all_ones, -mask_shift_len)).bool()[:, :, None]  # -1
         else:
-            dec_attn_mask = torch.triu(
-                word_emb.new_ones(qlen, klen), diagonal=1+mlen).bool()[:,:,None]
-
+            dec_attn_mask = torch.triu(word_emb.new_ones(qlen, klen), diagonal=1 + mlen).bool()[:, :, None]
 
         hids = []
-        pos_seq = torch.arange(klen-1, -1, -1.0, device=word_emb.device, 
-                                dtype=word_emb.dtype)
+        pos_seq = torch.arange(klen - 1, -1, -1.0, device=word_emb.device, dtype=word_emb.dtype)
         if self.clamp_len > 0:
             pos_seq.clamp_(max=self.clamp_len)
         pos_emb = self.pos_emb(pos_seq)
@@ -393,9 +376,8 @@ class MemTransformerLM(nn.Module):
 
         for i, layer in enumerate(self.layers):
             mems_i = None if mems is None else mems[i]
-            
-            core_out = layer(core_out, pos_emb, self.r_w_bias,
-                    self.r_r_bias, dec_attn_mask=dec_attn_mask, mems=mems_i)
+
+            core_out = layer(core_out, pos_emb, self.r_w_bias, self.r_r_bias, dec_attn_mask=dec_attn_mask, mems=mems_i)
             hids.append(core_out)
 
         core_out = self.drop(core_out)
@@ -404,14 +386,16 @@ class MemTransformerLM(nn.Module):
         return core_out, new_mems
 
     def generate(self, data, *mems):
-        if not mems: mems = self.init_mems()
+        if not mems:
+            mems = self.init_mems()
         hidden, new_mems = self._forward(data, mems=mems)
         predict = self.linear_proj(hidden[-1:])
         return predict, new_mems
 
     def forward(self, data, target, mask, *mems):
-        if not mems: mems = self.init_mems()
-        
+        if not mems:
+            mems = self.init_mems()
+
         tgt_len = target.size(0)
         hidden, new_mems = self._forward(data, mems=mems)
 
